b6265e32235fb15ae050a59e2a2663f3753d37ba
tavares2401ryan-lab <tavares2401ryan@gmail.com>
2026-03-02 08:16:46 +0000

    lista do mercadinho

---
+++ lista.py
@@ -0,0 +1,12 @@
+produtos = [
+    (1, "Arroz", 1.99, 50, "Alimentação"),
+    (2, "Leite", 0.89, 30, "Laticinios"),
+    (3, "Detergente", 2.49, 20, "Limpeza"),
+    (4, "Pão", 0.25, 100, "Padaria"),
+    (5, "Cafe", 3.99, 15, "Bebidas"),
+    (6, "Açúcar", 1.20, 40, "Alimentação"),
+    (7, "Champô", 4.50, 18, "Higiene"),
+    (8, "Massa", 1.10, 60, "Alimentação"),
+    (9, "Sumo", 1.75, 25, "Bebidas"),
+    (10, "Sabonete", 0.99, 35, "Higiene")
+]
